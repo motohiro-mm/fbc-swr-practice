b947d16d2879193a9ec66010287e72f6ff4306c0
motohiro-mm <polarbear.dx@gmail.com>
2024-04-16 22:02:00 +0900

    eslint,prettier実行

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import "./App.css";
-import useSWR from 'swr';
+import useSWR from "swr";
 
 function App() {
   const url = "https://httpstat.us/200?sleep=2000";
   const headers = { Accept: "application/json" };
 
-  const fetcher = (url) => fetch(url, {headers: headers}).then(res => res.json())
-  const { data, error, isLoading } = useSWR(url,fetcher)
+  const fetcher = (url) =>
+    fetch(url, { headers: headers }).then((res) => res.json());
+  const { data, error, isLoading } = useSWR(url, fetcher);
 
-  if (error) return <p>Failed to load.</p>
-  if (isLoading) return <p>Loading...</p>
+  if (error) return <p>Failed to load.</p>;
+  if (isLoading) return <p>Loading...</p>;
   return <>{data && <p>Status : {data.description}</p>}</>;
 }
 
