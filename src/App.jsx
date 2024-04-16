7c53731b352cf7ebaf944291b11f8f29c2d2c802
motohiro-mm <polarbear.dx@gmail.com>
2024-04-16 21:56:36 +0900

    useSWRを使ってエラーとローディング時にメッセージが表示されるよう実装

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-import { useState } from "react";
 import "./App.css";
+import useSWR from 'swr';
 
 function App() {
   const url = "https://httpstat.us/200?sleep=2000";
   const headers = { Accept: "application/json" };
 
-  const [status, setStatus] = useState("");
+  const fetcher = (url) => fetch(url, {headers: headers}).then(res => res.json())
+  const { data, error, isLoading } = useSWR(url,fetcher)
 
-  fetch(url, { headers })
-    .then((res) => res.json())
-    .then((json) => setStatus(json.description));
-
-  return <>{status && <p>Status : {status}</p>}</>;
+  if (error) return <p>Failed to load.</p>
+  if (isLoading) return <p>Loading...</p>
+  return <>{data && <p>Status : {data.description}</p>}</>;
 }
 
 export default App;
